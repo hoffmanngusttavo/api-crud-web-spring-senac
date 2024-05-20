dd111ac42838505e9a40a1cd61c13da4c8471285
Gustavo H. <hoffmann.gusttavo@gmail.com>
2024-05-20 20:09:22 -0300

    #[- jenkins file]

--- src/main/java/com/pessoa/senac/api/crud/controller/PessoaController.java
+++ src/main/java/com/pessoa/senac/api/crud/controller/PessoaController.java
@@ -4,7 +4,6 @@ import com.pessoa.senac.api.crud.model.Pessoa;
 import com.pessoa.senac.api.crud.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
